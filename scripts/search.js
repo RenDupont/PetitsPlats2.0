213220afcb7824e544a49ec8ad201f036394dcd9
Dupont <zeshin59@gmail.com>
2023-07-05 14:25:42 +0200

    2nd search version

--- scripts/search.js
+++ scripts/search.js
@@ -10,17 +10,31 @@ function search (query, recipes) {
   let filteredList = [...recipes];
 
   function searchMainBar () {
-    const regex = /^[a-zA-Z\s]+$/;
+    const filteredList = [];
+    const regex = /^[a-zA-Z]+$/;
+
     if (regex.test(query)) {
-      filteredList = filteredList.filter(function (recipe) {
-        return (
-          recipe.name.toLowerCase().includes(query) ||
-              recipe.ingredients.some(function (list) {
-                return list.ingredient.toLowerCase().includes(query);
-              }) ||
-              recipe.description.toLowerCase().includes(query)
-        );
-      });
+      for (let i = 0; i < recipes.length; i++) {
+        const recipe = recipes[i];
+        const lowerCaseName = recipe.name.toLowerCase();
+        const lowerCaseDescription = recipe.description.toLowerCase();
+        let found = false;
+
+        if (lowerCaseName.includes(query) || lowerCaseDescription.includes(query)) {
+          found = true;
+        } else {
+          for (let j = 0; j < recipe.ingredients.length; j++) {
+            const ingredient = recipe.ingredients[j].ingredient.toLowerCase();
+            if (ingredient.includes(query)) {
+              found = true;
+              break;
+            }
+          }
+        }
+        if (found) {
+          filteredList.push(recipe);
+        }
+      }
     }
     return filteredList;
   }
